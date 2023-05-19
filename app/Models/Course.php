3bdb36018df87b48698431f4a3d96a33e8735ed5
Petar2518 <pm20180025@student.fon.bg.ac.rs>
2023-05-19 19:37:28 +0200

    first commit

---
+++ app/Models/Course.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'level',
+        'description',
+    ];
+
+    public function language(){
+        return $this->belongsTo(Language::class);
+    }
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
